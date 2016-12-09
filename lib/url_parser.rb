8cfba30de456f86ee9b38507ca664cc3318d8184
Milan Lamote <milanlamote@hotmail.com>
2016-12-09 15:18:08 +0100

    added some tests for paths, and long paths (doesnt work yet)

--- lib/url_parser.rb
+++ lib/url_parser.rb
@@ -2,5 +2,33 @@
 
 class UrlParser
 
+  private
+
+  attr_accessor :url
+
+
+  public
+
+  def initialize url
+    @url = url
+  end
+
+  def host
+    (split "://").last
+  end
+
+  def scheme
+    (split "://").first
+  end
+
+  def path
+    (host.split "/").last
+  end
+
+  private
+
+  def split delimiter
+    url.split delimiter
+  end
 
 end
--- test/url_parser.rb
+++ test/url_parser.rb
@@ -5,7 +5,22 @@ class UrlParserTest < Minitest::Test
 
   def test_hostname
     url = UrlParser.new "http://vives.be"
-    assert_equals "vives.be", url.host
+    assert_equal "vives.be", url.host
+  end
+
+  def test_scheme
+    url = UrlParser.new "https://labict.be"
+    assert_equal "https", url.scheme
+  end
+
+  def test_path
+    url = UrlParser.new "http://youtube.com/skrimbler"
+    assert_equal "skrimbler", url.path
+  end
+
+  def test_long_path
+    url = UrlParser.new "http://youtube.com/skrimbler/views/0"
+    assert_equal "skrimbler/views/0", url.path
   end
 
 end
